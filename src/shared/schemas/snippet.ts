fcf3f099bba01e141c0522d0409344c28327f866
Harvey Balaam <harvey.balaam@pimberly.com>
2026-09-03 21:56:35 +0100

    feat: use zod to define snippet schemas

---
+++ src/shared/schemas/snippet.ts
@@ -0,0 +1,76 @@
+import * as z from "zod";
+
+export const snippetSchema = z
+  .strictObject({
+    id: z.string().trim().nonempty().readonly(),
+    dateCreated: z.iso.datetime().readonly(),
+    dateLastUpdated: z.iso.datetime(),
+    dateLastUsed: z.iso.datetime(),
+    name: z.string().trim().nonempty(),
+    pinned: z.boolean(),
+    value: z.string().trim().nonempty(),
+  })
+  .required();
+
+export const snippetContentSchema = snippetSchema.pick({
+  name: true,
+  pinned: true,
+  value: true,
+});
+
+export const snippetUpdateBodySchema = snippetSchema.omit({
+  id: true,
+  dateCreated: true,
+  dateLastUpdated: true,
+});
+
+export const snippetCollectionSchema = z.strictObject({
+  snippets: z.array(snippetSchema).optional(), // TODO: make this required?
+});
+
+export const baseSnippetResponseSchema = z.strictObject({
+  success: z.boolean(),
+});
+
+export const createSnippetResponseSchema = baseSnippetResponseSchema.safeExtend(
+  {
+    snippet: snippetSchema.optional(),
+  },
+);
+
+export const getSnippetResponseSchema = baseSnippetResponseSchema.safeExtend({
+  snippet: snippetSchema.optional(),
+});
+
+export const getAllSnippetsResponseSchema =
+  baseSnippetResponseSchema.safeExtend({
+    snippets: z.array(snippetSchema).optional(),
+  });
+
+export const updateSnippetResponseSchema = baseSnippetResponseSchema.safeExtend(
+  {
+    snippet: snippetSchema.optional(),
+  },
+);
+
+export type Snippet = z.infer<typeof snippetSchema>;
+
+export type SnippetContent = z.infer<typeof snippetContentSchema>;
+
+export type SnippetCollection = z.infer<typeof snippetCollectionSchema>;
+
+export type SnippetUpdateBody = z.infer<typeof snippetUpdateBodySchema>;
+
+export type BaseSnippetResponse = z.infer<typeof baseSnippetResponseSchema>;
+
+export type CreateSnippetResponse = z.infer<typeof createSnippetResponseSchema>;
+
+export type GetSnippetResponse = z.infer<typeof getSnippetResponseSchema>;
+
+export type GetAllSnippetsResponse = z.infer<
+  typeof getAllSnippetsResponseSchema
+>;
+
+export type UpdateSnippetResponse = z.infer<typeof updateSnippetResponseSchema>;
+
+export type DeleteSnippetResponse = z.infer<typeof baseSnippetResponseSchema>;
