a2ca981824357e63e8e42b9c620a478905626937
Harvey Balaam <harvey.balaam@pimberly.com>
2026-09-08 22:15:08 +0100

    feat: add get snippets error and loading states

--- src/ui/components/SnippetList/index.tsx
+++ src/ui/components/SnippetList/index.tsx
@@ -3,14 +3,38 @@ import SnippetListSection from "./SnippetListSection";
 import getRecentSnippets from "../../utils/get-recent-snippets";
 
 export default function SnippetList() {
-  const { data: getAllSnippetsResponse } = useQuery({
+  const {
+    data: getAllSnippetsResponse,
+    error,
+    isError,
+    isPending,
+  } = useQuery({
     queryKey: ["snippets"],
-    queryFn: window.api.getAllSnippets,
+    queryFn: async () => {
+      const response = await window.api.getAllSnippets();
+      if (response.success) return response;
+
+      throw new Error("Failed to fetch snippets");
+    },
   });
   const fetchedSnippets = getAllSnippetsResponse?.snippets ?? [];
 
   const pinnedSnippets = fetchedSnippets.filter((snippet) => snippet.pinned);
   const recentSnippets = getRecentSnippets(fetchedSnippets);
+
+  if (isPending) {
+    return (
+      <div className="text-overlay-0 px-4 pb-2 text-sm">
+        Loading snippets...
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className="text-overlay-0 px-4 pb-2 text-sm">{`An error occurred while loading snippets: ${error.message}`}</div>
+    );
+  }
 
   return (
     <div className="flex flex-col gap-2 px-2 pb-4">
