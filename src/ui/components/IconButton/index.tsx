1ff4e27d339f201ffcdcc3d70c1e7748272678d7
Harvey Balaam <harvey.balaam@pimberly.com>
2026-09-10 20:48:00 +0100

    fix: use full class names to ensure tailwind identifies them

--- src/ui/components/IconButton/index.tsx
+++ src/ui/components/IconButton/index.tsx
@@ -1,9 +1,14 @@
 import type { LucideIcon } from "lucide-react";
 import type { Colour } from "../../types/colour";
 
+type IconButtonColours = Extract<
+  Colour,
+  "red" | "peach" | "green" | "sapphire"
+>;
+
 interface IconButtonProps {
   disabled?: boolean;
-  colour: Colour;
+  colour: IconButtonColours;
   icon: LucideIcon;
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
   type?: React.ButtonHTMLAttributes<HTMLButtonElement>["type"];
@@ -16,9 +21,17 @@ export default function IconButton({
   onClick,
   type = "button",
 }: IconButtonProps) {
+  const colourVariants = {
+    red: "bg-red/25 text-red hover:bg-red focus-visible:outline-red",
+    peach: "bg-peach/25 text-peach hover:bg-peach focus-visible:outline-peach",
+    green: "bg-green/25 text-green hover:bg-green focus-visible:outline-green",
+    sapphire:
+      "bg-sapphire/25 text-sapphire hover:bg-sapphire focus-visible:outline-sapphire",
+  } satisfies Record<IconButtonColours, string>;
+
   return (
     <button
-      className={`rounded-md bg-${colour}/25 text-${colour} cursor-pointer text-center p-1 hover:bg-${colour} hover:text-crust focus-visible:outline-2 focus-visible:outline-${colour} disabled:cursor-not-allowed disabled:bg-overlay-2/25 disabled:text-overlay-2 transition duration-150`}
+      className={`${colourVariants[colour]} rounded-md cursor-pointer text-center p-1 hover:text-crust focus-visible:outline-2 disabled:cursor-not-allowed disabled:bg-overlay-2/25 disabled:text-overlay-2 transition duration-150`}
       disabled={disabled}
       onClick={onClick}
       type={type}
