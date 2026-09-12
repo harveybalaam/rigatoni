545cfe68e1b02639cd876b45716e12c7c45c27ef
Harvey Balaam <harvey.balaam@pimberly.com>
2026-09-12 17:14:21 +0100

    feat: delete snippets

--- src/ui/components/Snippet/index.tsx
+++ src/ui/components/Snippet/index.tsx
@@ -1,15 +1,24 @@
 import { PencilIcon, Trash2Icon } from "lucide-react";
 import type { Snippet } from "../../../shared/schemas/snippet";
+import { useDeleteSnippetMutation } from "../../api/snippet/mutations";
 
 type SnippetProps = Pick<Snippet, "id" | "name" | "value"> & {
   onEditClick: (snippetId: Snippet["id"]) => void;
 };
 
 export default function Snippet({ id, onEditClick, name }: SnippetProps) {
+  const { isError: isDeleteError, mutate: deleteSnippetMutation } =
+    useDeleteSnippetMutation();
+
   return (
     <div className=" bg-crust text-primary hover:text-mauve hover:bg-base flex gap-1 justify-between items-center p-2 m-0 rounded-lg has-focus-visible:bg-base has-focus-visible:[&>.action]:inline hover:[&>.action]:inline">
       <button className="rounded-md grow overflow-x-scroll text-start focus-visible:outline-2 focus-visible:outline-mauve focus-visible:text-mauve hover:cursor-pointer">
         {name}
+        {isDeleteError && (
+          <p className="italic text-red text-xs leading-tight">
+            An error occurred while deleting the snippet
+          </p>
+        )}
       </button>
       <button
         className="action hidden rounded-md bg-peach/25 text-peach cursor-pointer text-center p-1 hover:bg-peach hover:text-base focus-visible:outline-2 focus-visible:outline-peach transition duration-150"
@@ -17,7 +26,10 @@ export default function Snippet({ id, onEditClick, name }: SnippetProps) {
       >
         <PencilIcon size={16} />
       </button>
-      <button className="action hidden group-focus-within:inline group-hover:inline rounded-md bg-red/25 text-red cursor-pointer text-center p-1 hover:bg-red hover:text-base focus-visible:outline-2 focus-visible:outline-red transition duration-150">
+      <button
+        className="action hidden group-focus-within:inline group-hover:inline rounded-md bg-red/25 text-red cursor-pointer text-center p-1 hover:bg-red hover:text-base focus-visible:outline-2 focus-visible:outline-red transition duration-150"
+        onClick={() => deleteSnippetMutation(id)}
+      >
         <Trash2Icon size={16} />
       </button>
     </div>
