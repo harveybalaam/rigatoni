f32b3d91a774dd7b9df373e89badfdb95bfd8028
Harvey Balaam <harvey.balaam@pimberly.com>
2026-09-12 11:41:49 +0100

    feat: update snippet mutation

--- src/ui/api/snippet/mutations.ts
+++ src/ui/api/snippet/mutations.ts
@@ -2,7 +2,15 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import {
   createSnippetResponseSchema,
   snippetContentSchema,
+  snippetUpdateBodySchema,
+  updateSnippetResponseSchema,
+  type Snippet,
 } from "../../../shared/schemas/snippet";
+
+interface UpdateSnippetMutationParams {
+  event: React.SubmitEvent<HTMLFormElement>;
+  snippetId: Snippet["id"];
+}
 
 async function createSnippet(event: React.SubmitEvent<HTMLFormElement>) {
   event.preventDefault();
@@ -30,6 +38,53 @@ async function createSnippet(event: React.SubmitEvent<HTMLFormElement>) {
   if (!parsedResponse.success) throw new Error("Failed create snippet");
 
   return parsedResponse.data.snippet;
+}
+
+async function updateSnippet(
+  event: React.SubmitEvent<HTMLFormElement>,
+  snippetId: Snippet["id"],
+) {
+  event.preventDefault();
+
+  const rawFormData = Object.fromEntries(new FormData(event.target));
+
+  // coerce presence/absence of FormData's 'on' to boolean
+  const transformedFormData = {
+    ...rawFormData,
+    pinned: rawFormData.pinned === "on",
+  };
+
+  const parsedFormData = snippetUpdateBodySchema.safeParse(transformedFormData);
+
+  if (parsedFormData.error || !parsedFormData.data) {
+    throw new Error("Invalid form data");
+  }
+
+  const response = await window.api.updateSnippet(
+    parsedFormData.data,
+    snippetId,
+  );
+  const parsedResponse = updateSnippetResponseSchema.safeParse(response);
+  if (parsedResponse.error) {
+    throw new Error("Invalid response format");
+  }
+
+  if (!parsedResponse.success) throw new Error("Failed create snippet");
+
+  return parsedResponse.data.snippet;
+}
+
+export function useUpdateSnippetMutation() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ event, snippetId }: UpdateSnippetMutationParams) =>
+      updateSnippet(event, snippetId),
+    onSuccess: (_data, variables) =>
+      queryClient.invalidateQueries({
+        queryKey: ["snippets", variables.snippetId],
+      }),
+  });
 }
 
 export function useCreateSnippetMutation() {
