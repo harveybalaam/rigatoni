845af85ad39b8adfcd398abbb91e0e04552ff77e
Harvey Balaam <harvey.balaam@pimberly.com>
2026-09-12 22:59:29 +0100

    feat: show copy success message

--- src/ui/components/Snippet/index.tsx
+++ src/ui/components/Snippet/index.tsx
@@ -1,6 +1,7 @@
 import { PencilIcon, Trash2Icon } from "lucide-react";
 import type { Snippet } from "../../../shared/schemas/snippet";
 import { useDeleteSnippetMutation } from "../../api/snippet/mutations";
+import { useEffect, useState } from "react";
 
 type SnippetProps = Pick<Snippet, "id" | "name" | "value"> & {
   onEditClick: (snippetId: Snippet["id"]) => void;
@@ -12,11 +13,24 @@ export default function Snippet({
   onEditClick,
   value,
 }: SnippetProps) {
+  const [showCopySuccess, setShowCopySuccess] = useState(false);
+
   const { isError: isDeleteError, mutate: deleteSnippetMutation } =
     useDeleteSnippetMutation();
 
+  useEffect(() => {
+    if (!showCopySuccess) return;
+
+    const timeoutId = setTimeout(() => {
+      setShowCopySuccess(false);
+    }, 1000);
+
+    return () => clearTimeout(timeoutId);
+  }, [showCopySuccess]);
+
   const onCopyClick = async () => {
     await navigator.clipboard.writeText(value);
+    setShowCopySuccess(true);
   };
 
   return (
@@ -25,12 +39,19 @@ export default function Snippet({
         className="rounded-md grow overflow-x-scroll text-start focus-visible:outline-2 focus-visible:outline-mauve focus-visible:text-mauve hover:cursor-pointer"
         onClick={onCopyClick}
       >
-        {name}
-        {isDeleteError && (
-          <p className="italic text-red text-xs leading-tight">
-            An error occurred while deleting the snippet
+        <span className="flex gap-3 items-center">
+          {name}
+          {isDeleteError && (
+            <p className="italic text-red text-xs leading-tight">
+              An error occurred while deleting the snippet
+            </p>
+          )}
+          <p
+            className={`invisible opacity-0 text-xs text-green transition ease-out duration-200${showCopySuccess ? " visible opacity-100" : ""}`}
+          >
+            Copied
           </p>
-        )}
+        </span>
       </button>
       <button
         className="action hidden rounded-md bg-peach/25 text-peach cursor-pointer text-center p-1 hover:bg-peach hover:text-base focus-visible:outline-2 focus-visible:outline-peach transition duration-150"
