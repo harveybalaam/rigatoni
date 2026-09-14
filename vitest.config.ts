f6b54a22005aefc249f789f5709025fc45af47e9
Harvey Balaam <harvey.balaam@pimberly.com>
2026-09-14 22:20:43 +0100

    chore: add vitest config

---
+++ vitest.config.ts
@@ -0,0 +1,8 @@
+import { configDefaults, defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    clearMocks: true,
+    exclude: [...configDefaults.exclude, "**/dist-*/**"],
+  },
+});
