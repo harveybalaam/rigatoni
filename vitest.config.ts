bb192eeaafd0579d92d4c5de082ac2a19a76c659
Harvey Balaam <harvey.balaam@pimberly.com>
2026-09-15 21:48:17 +0100

    test: add coverage reporting

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,12 @@ import { configDefaults, defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     clearMocks: true,
+    coverage: {
+      ...configDefaults.coverage,
+      provider: "v8",
+      enabled: true,
+      exclude: ["**/dist-*/**"],
+    },
     exclude: [...configDefaults.exclude, "**/dist-*/**"],
   },
 });
